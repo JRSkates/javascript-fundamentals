7ac69e0778cafda04ba2edc10cd70b0565a7b6b6
JRSkates <skatesproduction@gmail.com>
2023-05-22 13:30:07 +0100

    First Commit

---
+++ hello.js
@@ -0,0 +1,9 @@
+console.log("Hello - I am a file!");
+
+const sum = 2 + 2
+console.log('2 + 2 = ' + sum);
+
+const greeting = 'Hello ';
+const someName = 'Makers';
+
+console.log(greeting + someName);
