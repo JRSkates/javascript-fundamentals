945e0238fd9d4557c2801549826d68906cf51fea
JRSkates <skatesproduction@gmail.com>
2023-05-22 13:51:30 +0100

    Export Modules Practice

--- hello.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello - I am a file!");
-
-const sum = 2 + 2
-console.log('2 + 2 = ' + sum);
-
-const greeting = 'Hello ';
-const someName = 'Makers';
-
-console.log(greeting + someName);
---
+++ phase-one/hello.js
@@ -0,0 +1,28 @@
+console.log("Hello - I am a file!");
+
+const sum = 2 + 2;
+console.log("2 + 2 = " + sum);
+
+const greeting = "Hello ";
+const someName = "Makers";
+
+console.log(greeting + someName);
+
+const printMessage = (message) => {
+  console.log(message);
+};
+
+const doSimpleSum = (num1, num2) => {
+  const result = num1 + num2;
+  return result;
+};
+
+const returnFour = () => {
+  return 4;
+};
+
+const thisIsFour = returnFour;
+
+printMessage("My first JavaScript function!");
+doSimpleSum(36, 45);
+console.log(thisIsFour());
