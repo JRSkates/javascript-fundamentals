230f6df8d634a6a3b31e08e2452f090657e75104
JRSkates <skatesproduction@gmail.com>
2023-05-25 16:28:48 +0100

    Fetch Pokemon API

---
+++ phase-two-last/api.js
@@ -0,0 +1,33 @@
+const extractData = (data) => {
+  const pokemon = {};
+  
+  pokemon.name = data.name;
+  pokemon.id = data.id;
+  pokemon.height = data.height;
+  pokemon.weight = data.weight;
+  pokemon.type = data.types.map((typeData) => typeData.type.name);
+  
+  return pokemon;
+}
+
+const fetchPokemon = (pokeName) => {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    return extractData(data);
+  })
+  .then((pokemon) => {
+    return pokemon;
+  })
+};
+
+const showPokemon = (name) => {
+  fetchPokemon(name).then((pokemon) => console.log(pokemon));
+}
+
+showPokemon('pikachu')
+// { name: 'pikachu', id: 25, height: 4, weight: 60, type: [ 'electric' ] }
+
+module.exports = fetchPokemon;
